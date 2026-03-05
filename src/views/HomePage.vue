bc1380b4273477c50b2ad36e418cc23d122ab941
ccontre03 <christophercontreras23@gmail.com>
2026-03-04 19:53:42 -0600

    Cleanup

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-const title = ref('Hello There ✋')
+
 </script>
 
 <template>
-  <main class="flex flex-1 items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <main class="flex flex-1 items-center justify-center">Hello There</main>
 </template>
